85849d6c88844cfd7cbe7a642fc8e2c75d3b3cf7
matheuscarpegiane <matheuscarpegianeleite08@gmail.com>
2025-06-15 00:07:04 -0300

    Add files via upload

---
+++ ageverificationv01.py
@@ -0,0 +1,10 @@
+
+name = input("Qual o seu nome? ")
+age = int(input("Qual a sua idade? "))
+
+if age > 18:
+    print("Ok.")
+else:
+    print("Idade não permitida. ")
+    exit()
+print("Verificação pos etapa. ")
